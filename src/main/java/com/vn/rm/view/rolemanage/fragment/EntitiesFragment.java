6f21c18ea7434cc84d1a7e050cc03f1c7112687e
Be Anh <hoanganhvu00124@gmail.com>
2025-11-03 11:04:36 +0700

    form thiet ke

---
+++ src/main/java/com/vn/rm/view/rolemanage/fragment/EntitiesFragment.java
@@ -0,0 +1,9 @@
+package com.vn.rm.view.rolemanage.fragment;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.jmix.flowui.fragment.Fragment;
+import io.jmix.flowui.fragment.FragmentDescriptor;
+
+@FragmentDescriptor("entities-fragment.xml")
+public class EntitiesFragment extends Fragment<VerticalLayout> {
+}
