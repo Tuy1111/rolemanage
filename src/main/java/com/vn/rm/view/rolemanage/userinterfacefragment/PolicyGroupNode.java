cd7ffea04478e373bf9e668a7d26f07c0aa402e4
Be Anh <hoanganhvu00124@gmail.com>
2025-11-13 10:08:21 +0700

    sua di em tuy

---
+++ src/main/java/com/vn/rm/view/rolemanage/userinterfacefragment/PolicyGroupNode.java
@@ -0,0 +1,7 @@
+package com.vn.rm.view.rolemanage.userinterfacefragment;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class PolicyGroupNode {
+}
---
+++ src/main/java/com/vn/rm/view/rolemanage/userinterfacefragment/UserInterfaceFragment.java
@@ -0,0 +1,9 @@
+package com.vn.rm.view.rolemanage.userinterfacefragment;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.jmix.flowui.fragment.Fragment;
+import io.jmix.flowui.fragment.FragmentDescriptor;
+
+@FragmentDescriptor("user-interface-fragment.xml")
+public class UserInterfaceFragment extends Fragment<VerticalLayout> {
+}
